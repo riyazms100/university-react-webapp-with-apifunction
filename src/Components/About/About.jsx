fbc0e4c5e6264ce2acc482b71fc7bab8fe43424c
Riyaz Ms <riyazms100@gmail.com>
2024-07-16 01:15:39 -0700

    first commit

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './About.css'
+import about_img from '../../assets/about.png'
+import play_icon from '../../assets/play-icon.png'
+
+const About = ({setPlayState}) => {
+  return (
+    <div className='about'>
+      <div className="about-left">
+        <div className="about-img-wrapper">
+          <img src={about_img} alt="About" className='about-img'/>
+          <img src={play_icon} alt="Play Icon" className='play-icon' onClick={()=>
+            {setPlayState(true)}}/>
+        </div>
+      </div>
+      <div className="about-right">
+        <h3>ABOUT UNIVERSITY</h3>
+        <h2>Nurturing Tomorrow's Leaders Today</h2>
+        <p>Embark on a transformative educational journey with our university's comprehensive education programs. Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education.</p>
+        <p>Embark on a transformative educational journey with our university's comprehensive education programs. Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education.</p>
+        <p>Embark on a transformative educational journey with our university's comprehensive education programs. Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education.</p>
+      </div>
+    </div>
+  )
+}
+
+export default About
